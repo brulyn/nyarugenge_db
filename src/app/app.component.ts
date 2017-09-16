5bdb4f4083418210b62ebe9a2c6790937dd9939e
Tharcille <Tharcillena@gmail.com>
2017-09-16 10:44:17 +0200

    skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
+declare var firebase: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  names = Array();
+
+  constructor(){
+    this.names = ["j"];
+  }
+
+  ngOnInit(){
+    firebase.database().ref('/main/').on('child_added',function(snapshot){
+      console.log(snapshot.val())
+    })
+  }
+
+  insert(){
+    firebase.database().ref('/main/')
+      .push({
+        nat_id: '123445666677',
+        first_name: 'Fabrice',
+        last_name: 'Higiro',
+        gender: 'M'
+       
+      })
+  }
+  get(){
+    var names = [];
+    firebase.database().ref('/main/').on('child_added',function(snapshot){
+      console.log(snapshot.val())
+      names.push(snapshot.val())  
+    })
+
+    this.names = names;
+  }
+
 }
---
+++ src/shared/main.ts
@@ -0,0 +1,25 @@
+export class main {
+
+    nat_id: string;
+    first_name: string;
+    last_name: string;
+    gender: string;
+    m_parent_id: string;
+    f_parent_id: string;
+    marital_status: string;
+    spouse_id: string;
+    umudugudu_id: string;
+    dob: string;
+    level_studies: string;
+    field_studies: string;
+    marriage_date: string;
+    baptism_date: string;
+    chruch_of_baptism: string;
+    joining_year: string;
+    itorero_yaturutsemo: string;
+    telephone: string;
+    email: string;
+    ubudehe: string;
+    imirimo_main_id: string;
+
+}
